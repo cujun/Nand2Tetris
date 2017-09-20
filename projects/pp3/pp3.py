1c13262ac016ce2a1b122f91f5476ec85ba11362
Junmin Choe <hyehwanet@gmail.com>
2017-09-20 15:41:40 +0900

    [Project 3] Implement formula parse tree

--- projects/pp3/pp3.py
+++ projects/pp3/pp3.py
@@ -7,13 +7,17 @@ from collections import namedtuple
 import operator
 from string import ascii_lowercase, ascii_uppercase
 
+import copy
+
+
+
 
 # Regular expression matching optional whitespace followed by a token
 # (if group 1 matches) or an error (if group 2 matches).
 # TOKEN_RE = re.compile(r'\s*(?:([A-Za-z01()~+*]|->)|(\S))')
 # TOKEN_RE = re.compile(r'(?:(([a-zA-Z][a-zA-Z0-9]*)|[()~+*]|->))')
-TOKEN_RE = re.compile(r'(?:([()~+*]|->|\S+))')
-VARIABLE_RE = re.compile(r'[a-zA-Z][a-zA-Z0-9]+')
+TOKEN_RE = re.compile(r'(?:([()~+*]|->|[^\s()~+*]+))')
+VARIABLE_RE = re.compile(r'[a-zA-Z][a-zA-Z0-9]*')
 
 # Special token indicating the end of the input string.
 TOKEN_END = '<end of input>'
@@ -33,11 +37,12 @@ BINARY_OPERATORS = {
             '->': lambda a, b: not a or b,
 }
 
-# Input pins
-INPUT_PINS = []
+Constant = namedtuple('Constant', 'value')
+Variable = namedtuple('Variable', 'name')
+UnaryOp = namedtuple('UnaryOp', 'op operand')
+BinaryOp = namedtuple('BinaryOp', 'left op right')
 
-# Nand gates which we'll write to output file
-NAND_GATES = []
+
 
 
 def tokenize(s):
@@ -50,24 +55,115 @@ def tokenize(s):
     yield TOKEN_END
 
 
+def parse(s):
+    """Parse s as a Boolean expression and return the parse tree."""
+    tokens = tokenize(s)        # Stream of tokens.
+    token = next(tokens)        # The current token.
+    print("toekn:[{}]".format(token))
+
+    def error(expected):
+        # Current token failed to match, so raise syntax error.
+        raise SyntaxError("Expected {} but found {!r}"
+                          .format(expected, token))
+
+    def match(valid_tokens):
+        # If the current token is found in valid_tokens, consume it
+        # and return True. Otherwise, return False.
+        nonlocal token
+        if token in valid_tokens:
+            token = next(tokens)
+            return True
+        else:
+            return False
+
+    def term():
+        # Parse a <Term> starting at the current token.
+        nonlocal token
+        t = token
+        if VARIABLE_RE.match(t):    # Not match()
+            INPUT_PINS.add(t)
+            token = next(tokens)    # Corner case!!
+            return Variable(name=t)
+        elif match('('):
+            tree = disjunction()
+            if match(')'):
+                return tree
+            else:
+                error("')'")
+        else:
+            error("term")
+
+    def unary_expr():
+        # Parse a <UnaryExpr> starting at the current token.
+        t = token
+        if match('~'):
+            operand = unary_expr()
+            return UnaryOp(op=UNARY_OPERATORS[t], operand=operand)
+        else:
+            return term()
+
+    def binary_expr(parse_left, valid_operators, parse_right):
+        # Parse a binary expression starting at the current token.
+        # Call parse_left to parse the left operand; the operator must
+        # be found in valid_operators; call parse_right to parse the
+        # right operand.
+        left = parse_left()
+        t = token
+        if match(valid_operators):
+            right = parse_right()
+            return BinaryOp(left=left, op=BINARY_OPERATORS[t], right=right)
+        else:
+            return left
+
+    def implication():
+        # Parse an <Implication> starting at the current token.
+        return binary_expr(unary_expr, '->', implication)
+
+    def conjunction():
+        # Parse a <Conjunction> starting at the current token.
+        return binary_expr(implication, '*', conjunction)
+
+    def disjunction():
+        # Parse a <Disjunction> starting at the current token.
+        return binary_expr(conjunction, '+', disjunction)
+
+    tree = disjunction()
+    if token != TOKEN_END:
+        error("end of input")
+    return tree
+
+
+
+
 if __name__ == '__main__':
     try:
         lines = list(filter(None, [line.rstrip('\n') for line in open(INPUT_FILE_NAME)]))
         for line in lines:
+            # Initializations
+            INPUT_PINS = set()  # Input pins
+            NAND_GATES = []     # Nand gates which we'll write to output file
             print()
-            # Parse current line
+
+            # Split current line into 'Name' and 'Formula' parts
             line_tokenized = list(filter(None, [l.strip() for l in line.split('=')]))
             if len(line_tokenized) != 2:    # SKIP condition
                 logging.warning("Unexpected input line exists. SKIP!!")
                 continue
             name = line_tokenized[0]; formula = line_tokenized[1]
-            formula_tokenized = tokenize(formula);  # Parse formula
             '''
-            for token in formula_tokenized:
-                print(token)
+            print("@@@@@@@@@")
+            tt = tokenize(formula)
+            for ttt in tt:
+                print(ttt)
+            print("@@@@@@@@@")
             '''
 
             # Build parse tree
+            print(parse(formula))
+            print("Input pins: {}".format(INPUT_PINS))
+
+            # Process parse tree to generate Nand gates
+            ## TO DO
 
             # Create chip
             f_output = open("{}.hdl".format(name), 'w')
@@ -76,7 +172,7 @@ if __name__ == '__main__':
             f_output.write("CHIP {} ".format(name))
             f_output.write("{\n")
             # TO DO: Print input pins
-            input_pins = "  IN " + "\n"
+            input_pins = "  IN " + ";\n"
             f_output.write(input_pins)
             f_output.write("  OUT out;\n\n")
             f_output.write("  PARTS:\n")
