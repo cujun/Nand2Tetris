781ff99360656bec32f5bd9027032540cf92255e
Junmin Choe <hyehwanet@gmail.com>
2017-10-18 14:41:00 +0900

    [Project 9] Implement 'First Pass'

--- projects/06/Assembler.py
+++ projects/06/Assembler.py
@@ -67,11 +67,47 @@ RE_COMMANDS = (
     ( 'RE_COMMENT', re.compile(r"^//") ),
 )
 
-# Symbol table
-symbol_table = dict()
+# Symbol table with some predefined symbols
+symbol_table = {
+    'SP'    : 0x0000,
+    'LCL'   : 0x0001,
+    'ARG'   : 0x0002,
+    'THIS'  : 0x0003,
+    'THAT'  : 0x0004,
+    'R0'    : 0x0000,
+    'R1'    : 0x0001,
+    'R2'    : 0x0002,
+    'R3'    : 0x0003,
+    'R4'    : 0x0004,
+    'R5'    : 0x0005,
+    'R6'    : 0x0006,
+    'R7'    : 0x0007,
+    'R8'    : 0x0008,
+    'R9'    : 0x0009,
+    'R10'   : 0x000A,
+    'R11'   : 0x000B,
+    'R12'   : 0x000C,
+    'R13'   : 0x000D,
+    'R14'   : 0x000E,
+    'R15'   : 0x000F,
+    'SCREEN': 0x4000,
+    'KBD'   : 0x6000,
+}
 
 
 
+
+def match_regex(s):
+    # Please make sure there's no whitespace in s.
+    for cmd_type, pattern in RE_COMMANDS:
+        if pattern.match(s):
+            return cmd_type
+    return None
+
+def is_decimal(s):
+    if s[0] in ('-', '+'):
+        return s[1:].isdecimal()
+    return s.isdecimal()
 
 def error_line():
     logging.warning("Unexpected assembly command.")
@@ -87,6 +123,7 @@ def main():
         lines = list(filter(None, [line.strip() for line in open(sys.argv[1])]))
         logging.info("Start assembling [{}].".format(sys.argv[1]))
         filename_asm = (sys.argv[1])[:-4]
+        addr_symbol = 0x0010
     except IndexError:
         logging.error("Please properly enter the name of assembly file as an argument.")
         return
@@ -94,21 +131,34 @@ def main():
         logging.error("Could not read input: [{}].".format(sys.argv[1]))
         return
 
-    for line in lines:
-        logging.debug(line)
-        curr = "".join(line.split())
-        for key, pattern in RE_COMMANDS:
-            if pattern.match(curr):
-                logging.debug(key)
     # First Pass. (Build symbol table + Syntax check)
     global symbol_table
     parsed_lines = []
     for line in lines:
         # Parse current line.
-        pass
+        curr = ''.join(line.split())
+        logging.debug(curr)
 
         # If new symbol found, register to symbol table.
-        pass
+        parsed_lines = []; parsed_line = None
+        typee = match_regex(curr)
+        if typee == 'RE_INST_A':
+            symbol = curr[1:]
+            parsed_line = [typee, symbol]
+        elif typee == 'RE_INST_C':
+            cmd_split = re.split(r"[=;]", curr)
+            parsed_line = [typee]
+            for s in cmd_split:
+                parsed_line.append(s)
+        elif typee == 'RE_LABEL':
+            symbol = curr[1:-1]
+            if (not is_decimal(symbol)) and (symbol not in symbol_table):
+                logging.debug("Add symbol [{}]".format(symbol))
+                symbol_table[symbol] = addr_symbol
+                addr_symbol += 1
+        if parsed_line:
+            logging.debug(parsed_line)
+            parsed_lines.append(parsed_line)
 
     # Second Pass. (Generate Hack binary code)
     '''
