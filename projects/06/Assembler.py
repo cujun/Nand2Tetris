dc9a5e16b5ad8ad4b99b214a5ecd8d9bc6a87698
Junmin Choe <hyehwanet@gmail.com>
2017-10-19 15:08:06 +0900

    [Project 9] Implement 'Second Pass'

--- projects/06/Assembler.py
+++ projects/06/Assembler.py
@@ -9,7 +9,7 @@ import re
 
 
 
-# Dicts for creating binary code corresponding C instruction
+# Costants for creating binary code corresponding C instruction
 BIN_COMP = {
     '0'     : '0101010',
     '1'     : '0111111',
@@ -41,6 +41,7 @@ BIN_COMP = {
     'D|M'   : '1010101',
 }
 BIN_DEST = {
+    'null'  : '000',
     'M'     : '001',
     'D'     : '010',
     'MD'    : '011',
@@ -50,6 +51,7 @@ BIN_DEST = {
     'AMD'   : '111',
 }
 BIN_JUMP = {
+    'null'  : '000',
     'JGT'   : '001',
     'JEQ'   : '010',
     'JGE'   : '011',
@@ -59,7 +61,7 @@ BIN_JUMP = {
     'JMP'   : '111',
 }
 
-# Regular expression matching assembly command
+# Regular expressions matching each assembly command
 RE_COMMANDS = (
     ( 'RE_INST_A', re.compile(r"^@(\d+|[a-zA-Z_.$:][\w.$:]*)$") ),
     ( 'RE_INST_C', re.compile(r"^([A?M?D?]+=)?[+\-!&|01AMD]+(;J[A-Z]{2})?$") ),
@@ -110,7 +112,7 @@ def is_decimal(s):
     return s.isdecimal()
 
 def error_line():
-    logging.warning("Unexpected assembly command.")
+    logging.warning("Format of some instruction is not correct, so generated hack file may be a garbage...")
 
 
 
@@ -121,56 +123,81 @@ def main():
     # Initializations.
     try:
         lines = list(filter(None, [line.strip() for line in open(sys.argv[1])]))
-        logging.info("Start assembling [{}].".format(sys.argv[1]))
+        logging.info("Start assembling <{}>.".format(sys.argv[1]))
         filename_asm = (sys.argv[1])[:-4]
         addr_inst = 0; addr_var = 0x0010
     except IndexError:
         logging.error("Please properly enter the name of assembly file as an argument.")
         return
     except FileNotFoundError:
-        logging.error("Could not read input: [{}].".format(sys.argv[1]))
+        logging.error("Could not read input: <{}>.".format(sys.argv[1]))
         return
 
     # First Pass. (Build symbol table + Syntax check)
     global symbol_table
-    parsed_lines = []
+    parsed_lines = list()
     for line in lines:
         # Parse current line.
         curr = re.split(r"//", line)
         curr = ''.join(curr[0].split())
-        logging.debug(curr)
+        logging.debug("[First Pass] curr <{}>".format(curr))
+        if len(curr) < 1:
+            continue
 
         # If new symbol found, register to symbol table.
-        parsed_lines = []; parsed_line = None
+        parsed_line = None
         typee = match_regex(curr)
         if typee == 'RE_INST_A':
             symbol = curr[1:]
             parsed_line = [typee, symbol]
         elif typee == 'RE_INST_C':
-            cmd_split = re.split(r"[=;]", curr)
             parsed_line = [typee]
-            for s in cmd_split:
-                parsed_line.append(s)
+            cmd_split = curr.split('=')
+            parsed_line.append(cmd_split[0] if len(cmd_split) > 1 else 'null')
+            if len(cmd_split) > 1:
+                cmd_split[0] = cmd_split[1]
+            cmd_split = cmd_split[0].split(';')
+            parsed_line.append(cmd_split[0])
+            parsed_line.append(cmd_split[1] if len(cmd_split) > 1 else 'null')
         elif typee == 'RE_LABEL':
             symbol = curr[1:-1]
             if (not is_decimal(symbol)) and (symbol not in symbol_table):
-                logging.debug("####Add symbol [{}:{}]".format(symbol, addr_inst))
+                logging.debug("[First Pass] Add symbol <{}:{}>".format(symbol, addr_inst))
                 symbol_table[symbol] = addr_inst
         if parsed_line:
-            logging.debug("####{}".format(parsed_line))
+            logging.debug("[First Pass] Code for later <{}>".format(parsed_line))
             parsed_lines.append(parsed_line)
             addr_inst += 1
+    logging.debug("[First Pass] Done")
+    # End of First Pass.
 
     # Second Pass. (Generate Hack binary code)
-    '''
     with open("{}.hack".format(filename_asm), 'w') as f_hack:
         for parsed_line in parsed_lines:
+            logging.debug("[Second Pass] Process <{}>".format(parsed_line))
             # Generate Hack binary code.
-            pass
-
+            bin_code = ''
+            if parsed_line[0] == 'RE_INST_A':
+                bin_code = "0"
+                if is_decimal(parsed_line[1]):
+                    bin_code += "{:015b}".format(int(parsed_line[1]))
+                elif parsed_line[1] in symbol_table:
+                    bin_code += "{:015b}".format(symbol_table[parsed_line[1]])
+                else:
+                    bin_code += "{:015b}".format(addr_var)
+                    logging.debug("[Second Pass] Add symbol <{}:{}>".format(parsed_line[1], addr_var))
+                    symbol_table[parsed_line[1]] = addr_var
+                    addr_var += 1
+            elif parsed_line[0] == 'RE_INST_C':
+                bin_code = "111{}{}{}".format(BIN_COMP.get(parsed_line[2], ''), BIN_DEST.get(parsed_line[1], ''), BIN_JUMP.get(parsed_line[3], ''))
             # Write binary code to .hack
-            pass
-    '''
+            if len(bin_code) == 16:
+                logging.debug("[Second Pass] Gen code <{}>".format(bin_code))
+                f_hack.write("{}\n".format(bin_code))
+            else:
+                error_line()
+    logging.debug("[Second Pass] Done")
+    # End of Second Pass.
     
     logging.info("Done.")
 
