0d7fef089582002104f2e3bcac96370f58bf2e93
Junmin Choe <hyehwanet@gmail.com>
2017-10-30 13:17:02 +0900

    [Project 10] Implement software fast multiplication

--- projects/pp10/Mult.asm
+++ projects/pp10/Mult.asm
@@ -1,32 +1,24 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
-
-// Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-
-// Put your code here.
-    @R0
-    D=M
-    @r0
-    M=D
     @R2
     M=0
-    @R1
-    D=M
     @counter
-    M=D
-    @INFINITE_LOOP
+    MD=1
+(LOOP)
+    @R1
+    D=D&M
+    @READY
     D;JEQ
-(MULTIPLY)
-    @r0
+    @R0
     D=M
     @R2
     M=D+M
+(READY)
+    @R0
+    D=M
+    M=D+M
     @counter
-    MD=M-1
-    @MULTIPLY
+    D=M
+    MD=D+M
+    @LOOP
     D;JNE
 (INFINITE_LOOP)
     @INFINITE_LOOP
